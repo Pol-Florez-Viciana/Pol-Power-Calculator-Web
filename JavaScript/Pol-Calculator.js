aae75c3a04e557f689600b7b2da4330f864d664b
Pol Flórez Viciana <38541039+Pol-Florez-Viciana@users.noreply.github.com>
2022-08-16 00:05:41 +0200

    Update 10.1

--- JavaScript/Pol-Calculator.js
+++ JavaScript/Pol-Calculator.js
@@ -647,11 +647,13 @@ function LOGSignos(Texto1,Texto2,Reiteraciones){
 										var Resta2 = RestaSignos(PreResultado2,Num1);
 										var TempoDivision = DivideSignos(Resta2,Resta1,Reitera);
 										var Resta3 = RestaSignos(StringUno,TempoDivision);
-										if (IsRealNumber(Num2) == true ){
-											var Digitos = StringNullString + GetRightNumber(Num2);
-											var LimiteStr = StringCero + StringComa + StringsCerosToLeft(Digitos, Digitos.length + Uno);
-											Resta3 = RestaSignos(Resta3,LimiteStr);
-										}	
+										if(IsMayor(Num2,StringDos) == false ){
+											if (IsRealNumber(Num2) == true ){
+												var Digitos = StringNullString + GetRightNumber(Num2);
+												var LimiteStr = StringCero + StringComa + StringsCerosToLeft(Digitos, Digitos.length + Uno);
+												Resta3 = RestaSignos(Resta3,LimiteStr);
+											}
+										}		
 										SubResultado = SumaSignos(PrimerParametro,Resta3);
 									}	
 								}else{
